c0477c4c63d979de967a05c229a0ba68c2649033
Mona B <web25Lucius@gmail.com>
2019-10-18 01:23:13 -0600

    almost finished. Refactor is done, and classes will be complete after I fix the last 3 console.log tests

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -37,8 +37,6 @@ class Student extends Person {
 }
 
 
-
-
 class Instructor extends Person {
     constructor (attributes){
         super (attributes); 
@@ -69,6 +67,85 @@ class ProjectManager extends Instructor{
             return `${this.name} debugs ${student.name}'s code on ${subject}`
         }
 }
+ 
+
+const Ash = new Student ({
+    name: 'Ashley',
+    age: 26, 
+    location: 'Utah', 
+    previousBackground: 'Automotive', 
+    className: 'web25', 
+    favSubjects: 'CSS' 
+}); 
+
+const Hui = new Student ({
+    name: 'Hui', 
+    age: 42, 
+    location: 'San Francisco', 
+    previousBackground: 'Grant Researcher', 
+    className: 'web25', 
+    favSubjects: 'HTML'
+}); 
+
+const Rikei = new Student ({
+    name: 'Rikei', 
+    age: 21, 
+    location: 'New York', 
+    previousBackground: 'Artist', 
+    className: 'web24', 
+    favSubjects: 'JavaScript, HTML, CSS3'
+}); 
+
+const BrittH = new Instructor ({
+    name: 'Britt Hemming', 
+    age: 28, 
+    location: 'Windsor', 
+    specialty: 'Advanced HTML/CSS/JS', 
+    favLanguage: 'CSS3', 
+    catchPhrase: 'Polar Vortex, Aye'
+}); 
+
+const JoshK = new Instructor ({
+    name: 'Josh Knell', 
+    age: 57, 
+    location: 'Provo', 
+    specialty: 'Orientation', 
+    favLanguage: 'React', 
+    catchPhrase: 'Did I do that?'
+}); 
+
+const RyanH = new Instructor ({
+    name: 'Ryan Hamblin', 
+    age: 38, 
+    location: 'Concord', 
+    specialty: 'Advanced JavaScript', 
+    favLanguage: 'Semantic HTML5', 
+    catchPhrase: 'Get in, Loser- we are goin coding'
+}); 
+
+const Itel = new ProjectManager ({
+    name: 'Itel D', 
+    age: 83, 
+    location: 'St. Louis', 
+    gradClassName: 'web17', 
+    favInstructor: 'Britt Hemming' 
+}); 
+
+const Marianna = new ProjectManager ({
+    name: 'Marianna A', 
+    age: 22, 
+    location: 'London', 
+    gradClassName: 'web4', 
+    favInstructor: 'Dan Frehner'
+}); 
 
 
+console.log(Ash); 
+console.log(Hui.speak()); 
+console.log(Rikei.listsSubjects());
+console.log(Britt.demo(ReactJSX));
+console.log(JoshK.grade(Ash, AccesibleHTML)); 
+console.log(RyanH.catchPhrase); 
+console.log(Itel.stantUp(web25_ah)); 
+console.log(Marianna.debugsCode(Hui, mediaBreaksAndYou));  
 
